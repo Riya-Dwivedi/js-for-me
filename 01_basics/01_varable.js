feecc8780b2fb4280d8e63ff5083569d1a1a88f6
Riya-Dwivedi <169047765+Riya-Dwivedi@users.noreply.github.com>
2025-11-12 12:51:07 +0000

    new learn

---
+++ 01_basics/01_varable.js
@@ -0,0 +1,12 @@
+const accountId=1234
+let accountEmail="123riya@gmail.com"
+var accountPass="27221116"//not used
+accountcity="jaipur"//it also possible but not use
+//  accountId=2
+accountPass="271116"
+accountcity="japur"
+ console.log(accountId)
+ console.table([accountId,
+ accountEmail,
+ accountPass,
+accountcity])
